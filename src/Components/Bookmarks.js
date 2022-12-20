5f60478266cbd11011403ca37938479c231a93f0
i-guerrero <isaacguerrero@pursuit.org>
2022-12-19 22:04:08 -0500

    Add cors and connect index to server/api

--- src/Components/Bookmarks.js
+++ src/Components/Bookmarks.js
@@ -1,8 +1,24 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Bookmark from "./Bookmark";
+import axios from "axios";
+const API = process.env.REACT_APP_API_URL;
 
 function Bookmarks() {
-  const [bookmarks] = useState([]);
+  const [bookmarks, setBookmarks] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get(`${API}/bookmarks`)
+      .then((response) => {
+        // console.log(response);
+        // console.log(response.data);
+        setBookmarks(response.data);
+      })
+      .catch((e) => {
+        console.error("catch", e);
+      });
+  }, []);
+
   return (
     <div className="Bookmarks">
       <section>
